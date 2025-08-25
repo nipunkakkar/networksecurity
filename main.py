e8cd66267af9e4fa50c83e30a6a7bdb66e32ed23
Nipun Kakkar <mrnipunkakkar@gmail.com>
2025-08-26 00:31:39 +0530

    Adding necessary files for model training

--- main.py
+++ main.py
@@ -21,20 +21,21 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
-        '''
+        
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiate the data Validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+        
         data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
         logging.info("data Transformation started")
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
-
+        
         logging.info("Model Training sstared")
         model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
         model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
@@ -42,7 +43,6 @@ if __name__=='__main__':
 
         logging.info("Model Training artifact created")
         
-        '''
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
